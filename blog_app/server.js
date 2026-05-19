d1becdbcb541ad65cb3cafcd418b99463de5d819
Brendan <brendanathoele@gmail.com>
2026-05-19 11:20:44 -0500

    Add npm server nodemon script, Posts.jsx file

--- blog_app/server.js
+++ blog_app/server.js
@@ -6,4 +6,18 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express();
-const pool = new Pool({})
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/users', async (req, res) => {
+    try {
+        const { rows } = await pool.query('SELECT * FROM lit');
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.listen(3000, () => console.log('Server running on port 3001'));
--- blog_app/src/components/Publish.jsx
+++ blog_app/src/components/Publish.jsx
@@ -1,15 +1,6 @@
 
 function Publish() {
-    return(
-        <div>
-            <h1>Publish</h1>
-            <form action="/upload-endpoint" method="POST" encType="multipart/form-data">
-                <label htmlFor="myFile">Select a file:</label>
-                <input type="file" />
-                <input type="submit" />
-            </form>
-        </div>
-    )
+    return
 }
 
 export default Publish
