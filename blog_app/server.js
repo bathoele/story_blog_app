713239852c3ad8c1fc55d37809d54474835bd017
Brendan <brendanathoele@gmail.com>
2026-05-22 09:31:19 -0500

    Initialize cors, express-session, and passport for oauth

--- blog_app/server.js
+++ blog_app/server.js
@@ -1,17 +1,25 @@
 import express from 'express'
-import { Pool } from 'pg'
+import session from 'express-session'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
-
+import passport from 'passport'
 
 dotenv.config()
 
 const app = express();
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(express.json());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// add auth middleware
 
 app.get('/api/posts', async (req, res) => {
     try {
